fe6c1f92192242f0740fd3d488c1a508266ae277
fl4shk <fl4shk@users.noreply.github.com>
2025-06-07 17:20:17 -0500

    again add back  registered PC incrementing!

--- hw/spinal/libsnowhouse/snowHousePipeStageAreas.scala
+++ hw/spinal/libsnowhouse/snowHousePipeStageAreas.scala
@@ -157,7 +157,7 @@ case class SnowHousePipeStageInstrFetch(
   }
   val rPrevRegPcThenNext = (
     RegNextWhen(
-      next=nextRegPc /*+ (cfg.instrMainWidth / 8)*/,
+      next=nextRegPc + (cfg.instrMainWidth / 8),
       cond=up.isFiring,
     )
     init(nextRegPc.getZero)
@@ -181,7 +181,7 @@ case class SnowHousePipeStageInstrFetch(
       myInstrCnt.jmp := rPrevInstrCnt.jmp + 1
     } otherwise {
       nextRegPcSetItCnt := 0x0
-      nextRegPc := rPrevRegPcThenNext + (cfg.instrMainWidth / 8)
+      nextRegPc := rPrevRegPcThenNext //+ (cfg.instrMainWidth / 8)
       myInstrCnt.fwd := rPrevInstrCnt.fwd + 1
     }
   }
