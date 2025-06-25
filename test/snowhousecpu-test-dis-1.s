755001398c3ee39238f906013082aa2a2b5053d9
fl4shk <fl4shk@users.noreply.github.com>
2025-06-25 10:14:27 -0500

    fmax debugging

--- test/snowhousecpu-test-dis-1.s
+++ test/snowhousecpu-test-dis-1.s
@@ -43,33 +43,97 @@ Disassembly of section .text:
   94:	03 00 61 78 	umod	r8, r6, r1
   98:	00 00 00 a0 	beq	r0, r0, 0 // pre #0x0
   9c:	00 00 00 90 
+  a0:	03 00 10 0a 	add	r10, r1, 3
+  a4:	03 00 10 0a 	add	r10, r1, 3
+  a8:	03 00 10 0a 	add	r10, r1, 3
+  ac:	03 00 10 0a 	add	r10, r1, 3
+  b0:	03 00 10 0a 	add	r10, r1, 3
+  b4:	03 00 10 0a 	add	r10, r1, 3
+  b8:	03 00 10 0a 	add	r10, r1, 3
+  bc:	03 00 10 0a 	add	r10, r1, 3
+  c0:	03 00 10 0a 	add	r10, r1, 3
+  c4:	03 00 10 0a 	add	r10, r1, 3
+  c8:	03 00 10 0a 	add	r10, r1, 3
+  cc:	03 00 10 0a 	add	r10, r1, 3
+  d0:	03 00 10 0a 	add	r10, r1, 3
+  d4:	03 00 10 0a 	add	r10, r1, 3
+  d8:	03 00 10 0a 	add	r10, r1, 3
+  dc:	03 00 10 0a 	add	r10, r1, 3
 
-000000a0 <_loop>:
-  a0:	00 00 30 86 	ldr	r6, r3, 0
-  a4:	00 00 00 a0 	bl	lr, 0 // pre #0x0
-  a8:	00 00 d0 9d 
-  ac:	04 00 31 86 	str	r6, r3, 4
-  b0:	04 00 30 03 	add	r3, r3, 4
-  b4:	ff ff 10 01 	add	r1, r1, -1
-  b8:	00 00 00 a0 	bl	lr, 0 // pre #0x0
-  bc:	00 00 d0 9d 
-  c0:	00 00 00 a0 	bne	r1, r0, 0 // pre #0x0
-  c4:	00 00 01 91 
+000000e0 <_loop>:
+  e0:	00 00 30 86 	ldr	r6, r3, 0
+  e4:	00 00 00 a0 	bl	lr, 0 // pre #0x0
+  e8:	00 00 d0 9d 
+  ec:	04 00 31 86 	str	r6, r3, 4
+  f0:	04 00 30 03 	add	r3, r3, 4
+  f4:	ff ff 10 01 	add	r1, r1, -1
+  f8:	00 00 00 a0 	bl	lr, 0 // pre #0x0
+  fc:	00 00 d0 9d 
+ 100:	00 00 00 a0 	bne	r1, r0, 0 // pre #0x0
+ 104:	00 00 01 91 
 
-000000c8 <_infin>:
-  c8:	00 00 00 a0 	beq	r0, r0, 0 // pre #0x0
-  cc:	00 00 00 90 
+00000108 <_infin>:
+ 108:	00 00 00 a0 	beq	r0, r0, 0 // pre #0x0
+ 10c:	00 00 00 90 
+ 110:	03 00 10 0b 	add	r11, r1, 3
+ 114:	03 00 10 0b 	add	r11, r1, 3
+ 118:	03 00 10 0b 	add	r11, r1, 3
+ 11c:	03 00 10 0b 	add	r11, r1, 3
+ 120:	03 00 10 0b 	add	r11, r1, 3
+ 124:	03 00 10 0b 	add	r11, r1, 3
+ 128:	03 00 10 0b 	add	r11, r1, 3
+ 12c:	03 00 10 0b 	add	r11, r1, 3
+ 130:	03 00 10 0b 	add	r11, r1, 3
+ 134:	03 00 10 0b 	add	r11, r1, 3
+ 138:	03 00 10 0b 	add	r11, r1, 3
+ 13c:	03 00 10 0b 	add	r11, r1, 3
+ 140:	03 00 10 0b 	add	r11, r1, 3
+ 144:	03 00 10 0b 	add	r11, r1, 3
+ 148:	03 00 10 0b 	add	r11, r1, 3
+ 14c:	03 00 10 0b 	add	r11, r1, 3
 
-000000d0 <_increment>:
-  d0:	01 00 60 06 	add	r6, r6, 1
-  d4:	00 00 d6 90 	jl	r0, lr
+00000150 <_increment>:
+ 150:	01 00 60 06 	add	r6, r6, 1
+ 154:	00 00 d6 90 	jl	r0, lr
+ 158:	03 00 10 0c 	add	r12, r1, 3
+ 15c:	03 00 10 0c 	add	r12, r1, 3
+ 160:	03 00 10 0c 	add	r12, r1, 3
+ 164:	03 00 10 0c 	add	r12, r1, 3
+ 168:	03 00 10 0c 	add	r12, r1, 3
+ 16c:	03 00 10 0c 	add	r12, r1, 3
+ 170:	03 00 10 0c 	add	r12, r1, 3
+ 174:	03 00 10 0c 	add	r12, r1, 3
+ 178:	03 00 10 0c 	add	r12, r1, 3
+ 17c:	03 00 10 0c 	add	r12, r1, 3
+ 180:	03 00 10 0c 	add	r12, r1, 3
+ 184:	03 00 10 0c 	add	r12, r1, 3
+ 188:	03 00 10 0c 	add	r12, r1, 3
+ 18c:	03 00 10 0c 	add	r12, r1, 3
+ 190:	03 00 10 0c 	add	r12, r1, 3
+ 194:	03 00 10 0c 	add	r12, r1, 3
 
-000000d8 <_divmod>:
-  d8:	01 00 61 77 	udiv	r7, r6, r1
-  dc:	03 00 61 78 	umod	r8, r6, r1
-  e0:	00 00 d6 90 	jl	r0, lr
+00000198 <_divmod>:
+ 198:	01 00 61 77 	udiv	r7, r6, r1
+ 19c:	03 00 61 78 	umod	r8, r6, r1
+ 1a0:	00 00 d6 90 	jl	r0, lr
+ 1a4:	03 00 10 04 	add	r4, r1, 3
+ 1a8:	03 00 10 04 	add	r4, r1, 3
+ 1ac:	03 00 10 04 	add	r4, r1, 3
+ 1b0:	03 00 10 04 	add	r4, r1, 3
+ 1b4:	03 00 10 04 	add	r4, r1, 3
+ 1b8:	03 00 10 04 	add	r4, r1, 3
+ 1bc:	03 00 10 04 	add	r4, r1, 3
+ 1c0:	03 00 10 04 	add	r4, r1, 3
+ 1c4:	03 00 10 04 	add	r4, r1, 3
+ 1c8:	03 00 10 04 	add	r4, r1, 3
+ 1cc:	03 00 10 04 	add	r4, r1, 3
+ 1d0:	03 00 10 04 	add	r4, r1, 3
+ 1d4:	03 00 10 04 	add	r4, r1, 3
+ 1d8:	03 00 10 04 	add	r4, r1, 3
+ 1dc:	03 00 10 04 	add	r4, r1, 3
+ 1e0:	03 00 10 04 	add	r4, r1, 3
 
-000000e4 <_irq_handler>:
-  e4:	01 00 a0 0a 	add	r10, r10, 1
-  e8:	0a 00 00 60 	ret	ira
+000001e4 <_irq_handler>:
+ 1e4:	01 00 a0 0a 	add	r10, r10, 1
+ 1e8:	0a 00 00 60 	ret	ira
 	...
