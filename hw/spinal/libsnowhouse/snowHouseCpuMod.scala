0004dd3bcf03455c27d5cc0887215a28e508f246
fl4shk <fl4shk@users.noreply.github.com>
2025-06-29 14:45:26 -0500

    fmax debugging

--- hw/spinal/libsnowhouse/snowHouseCpuMod.scala
+++ hw/spinal/libsnowhouse/snowHouseCpuMod.scala
@@ -3505,12 +3505,12 @@ object SnowHouseCpuWithDualRamSim extends App {
   val testIdxRange = (
     //0, //0
     //1, 1,
-    2, //2,
+    //2, //2,
     //3, //3,
     //4, 4,
     //5, //5,
     //6, //6
-    7, //7
+    7, 7
   )
   val instrRamKindArr = Array[Int](
     0,
