40f8d96661e4cbe6543f0344a8545b1024de8803
fl4shk <fl4shk@users.noreply.github.com>
2025-07-06 19:09:37 -0500

    new `LongDivMultiCycle` implementation: update test results

--- hw/spinal/libsnowhouse/snowHouseCpuMod.scala
+++ hw/spinal/libsnowhouse/snowHouseCpuMod.scala
@@ -3583,13 +3583,13 @@ object SnowHouseCpuWithDualRamSim extends App {
   //  "5",
   //)
   val testIdxRange = (
-    //0, //0,
-    //1, //1,
+    //0, 0,
+    1, //1,
     ////2, //2,
     ////3, //3,
     //4, //4,
     //5, 5,
-    //6, 6,
+    6, 6,
     7, 7
   )
   val instrRamKindArr = Array[Int](
