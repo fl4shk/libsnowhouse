1d2bd3c0f0e344e480ba5dad7b1e67771238fb0a
fl4shk <fl4shk@users.noreply.github.com>
2025-07-11 09:25:00 -0500

    hopefully can infer BRAM with two-cycle reads now!

--- hw/spinal/libsnowhouse/snowHouseCpuMod.scala
+++ hw/spinal/libsnowhouse/snowHouseCpuMod.scala
@@ -3586,13 +3586,13 @@ object SnowHouseCpuWithDualRamSim extends App {
   //  "5",
   //)
   val testIdxRange = (
-    //0, 0,
+    //0, //0,
     //1, 1,
-    //2, 2,
-    //3, 3,
-    4, //4,
-    5, //5,
-    //6, 6,
+    2, //2,
+    3, 3,
+    //4, 4,
+    //5, //5,
+    //6, //6,
     //7, 7
   )
   val instrRamKindArr = Array[Int](
