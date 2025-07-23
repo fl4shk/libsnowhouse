54f2fa985081fcae569bd6e64e5a9731c7bb9d5c
fl4shk <fl4shk@users.noreply.github.com>
2025-07-23 16:31:16 -0500

    okay, it works! all unit tests passing

--- hw/spinal/libsnowhouse/snowHouseCpuMod.scala
+++ hw/spinal/libsnowhouse/snowHouseCpuMod.scala
@@ -4045,8 +4045,8 @@ object SnowHouseCpuWithDualRamSim extends App {
   //  "5",
   //)
   val testIdxRange = (
-    0, 0,
-    //1, //1,
+    //0, 0,
+    //1, 1,
     //2, 2,
     //3, 3,
     //4, 4,
@@ -4055,13 +4055,13 @@ object SnowHouseCpuWithDualRamSim extends App {
     //7, 7,
     //8, 8,
     //9, 9,
-    //10, 10
+    10, 10
   )
   val instrRamKindArr = Array[Int](
     0,
-    //1,
-    //2,
-    //5,
+    1,
+    2,
+    5,
   )
   for (testIdx <- 0 to 10) {
     programStrArr += (
