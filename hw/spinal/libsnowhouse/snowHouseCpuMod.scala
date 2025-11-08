8c9be8bb868181766ae657842d9da9dff1a2430a
fl4shk <fl4shk@users.noreply.github.com>
2025-11-08 16:35:17 -0600

    tried changing test-11 slightly, and it still worked

--- hw/spinal/libsnowhouse/snowHouseCpuMod.scala
+++ hw/spinal/libsnowhouse/snowHouseCpuMod.scala
@@ -4795,7 +4795,7 @@ object SnowHouseCpuWithDualRamSim extends App {
   //  "5",
   //)
   val testIdxRange = (
-    0, 0,
+    //0, 0,
     //1, 1,
     //2, 2,
     //3, 3,
@@ -4806,13 +4806,13 @@ object SnowHouseCpuWithDualRamSim extends App {
     //8, 8,
     //9, 9,
     //10, 10,
-    //11, 11,
+    11, 11,
   )
   val instrRamKindArr = Array[Int](
-    //0,
+    0,
     //1,
     //2,
-    5,
+    //5,
   )
   for (testIdx <- 0 to 11) {
     programStrArr += (
