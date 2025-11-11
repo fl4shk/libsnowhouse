84735ca077d1d9170507bafb211f9080e7d191a9
fl4shk <fl4shk@users.noreply.github.com>
2025-11-11 13:40:44 -0600

    fmax debugging

--- hw/spinal/libsnowhouse/snowHouseCpuMod.scala
+++ hw/spinal/libsnowhouse/snowHouseCpuMod.scala
@@ -4857,7 +4857,7 @@ object SnowHouseCpuWithDualRamSim extends App {
   //  "5",
   //)
   val testIdxRange = (
-    0, 0,
+    //0, 0,
     //1, 1,
     //2, 2,
     //3, 3,
@@ -4866,7 +4866,7 @@ object SnowHouseCpuWithDualRamSim extends App {
     //6, 6,
     //7, 7,
     //8, 8,
-    //9, 9,
+    9, 9,
     //10, 10,
     //11, 11,
   )
