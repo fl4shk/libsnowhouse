3bae5239280037eee786f20d2a48a7dc619caedb
fl4shk <fl4shk@users.noreply.github.com>
2026-01-07 09:08:36 -0600

    several tests pass, but now I need to work on making sure test 4 works!

--- hw/spinal/libsnowhouse/snowHouseCpuMod.scala
+++ hw/spinal/libsnowhouse/snowHouseCpuMod.scala
@@ -5338,10 +5338,10 @@ object SnowHouseCpuWithDualRamSim extends App {
   //  "5",
   //)
   val testIdxRange = Array[Int](
-    //0, //0,
+    0, //0,
     //1, 1,
     //2, 2,
-    //3, 3,
+    3, 3,
     //4, 4,
     //5, 5,
     //6, 6,
@@ -5350,13 +5350,13 @@ object SnowHouseCpuWithDualRamSim extends App {
     //9, 9,
     //10, 10,
     //11, 11,
-    12, 12,
+    //12, 12,
   )
   val instrRamKindArr = Array[Int](
-    0,
+    //0,
     //1,
     //2,
-    //5,
+    5,
   )
   for (testIdx <- 0 to 12) {
     programStrArr += (
