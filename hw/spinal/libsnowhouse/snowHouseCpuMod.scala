5c5749de3d4f991d278fa2206a87f9d629d78f89
fl4shk <fl4shk@users.noreply.github.com>
2026-01-20 09:07:32 -0600

    it appears that there's a bug in IRQ handling with `io.lcvDbus` so that will be something to fix later I also still need to implement smaller-than-word-size loads/stores!

--- hw/spinal/libsnowhouse/snowHouseCpuMod.scala
+++ hw/spinal/libsnowhouse/snowHouseCpuMod.scala
@@ -5362,12 +5362,12 @@ object SnowHouseCpuWithDualRamSim extends App {
     false
   )
   val testIdxRange = Array[Int](
-    0, //0,
-    1, 1,
-    //2, 2,
+    //0, //0,
+    //1, 1,
+    //2, //2,
     //3, 3,
     //4, 4,
-    //5, //5,
+    5, //5,
     //6, //6,
     ////7, //7,
     //8, 8,
