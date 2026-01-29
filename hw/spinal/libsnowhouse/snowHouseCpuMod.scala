89051530fe8b745a15b2644983234d981dcc28f0
fl4shk <fl4shk@users.noreply.github.com>
2026-01-29 10:36:08 -0600

    `submodules/libcheesevoyage`: `git pull`

--- hw/spinal/libsnowhouse/snowHouseCpuMod.scala
+++ hw/spinal/libsnowhouse/snowHouseCpuMod.scala
@@ -5265,8 +5265,8 @@ object SnowHouseCpuWithDualRamSim extends App {
     false
   )
   val testIdxRange = Array[Int](
-    //0, //0,
-    1, //1,
+    0, //0,
+    //1, //1,
     //2, //2,
     //////////3, 3,
     //////4, 4,
