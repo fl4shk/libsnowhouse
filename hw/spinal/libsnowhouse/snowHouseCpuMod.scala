46cf7ca1481e4f35152c1e4f6bbd51e5dd499dfc
fl4shk <fl4shk@users.noreply.github.com>
2026-01-31 12:05:30 -0600

    all tests are passing!

--- hw/spinal/libsnowhouse/snowHouseCpuMod.scala
+++ hw/spinal/libsnowhouse/snowHouseCpuMod.scala
@@ -5319,7 +5319,7 @@ object SnowHouseCpuWithDualRamSim extends App {
     false
   )
   val testIdxRange = Array[Int](
-    0, 0,
+    0, //0,
     //1, //1,
     //2, //2,
     //////////3, 3,
@@ -5340,7 +5340,7 @@ object SnowHouseCpuWithDualRamSim extends App {
     0,
     //1,
     //2,
-    //5,
+    5,
   )
   for (testIdx <- 0 to testIdxRange(1)) {
     programStrArr += (
