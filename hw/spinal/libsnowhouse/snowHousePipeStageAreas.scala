f92ee820c88f0abf668bf792032a0ae9bf5540d3
fl4shk <fl4shk@users.noreply.github.com>
2026-03-08 13:27:13 -0500

    forwarding: correctness debugging

--- hw/spinal/libsnowhouse/snowHousePipeStageAreas.scala
+++ hw/spinal/libsnowhouse/snowHousePipeStageAreas.scala
@@ -10795,6 +10795,16 @@ case class SnowHousePipeStageWriteBack(
       //RegNext(myWbPayload(1), init=myWbPayload(1).getZero)
       myWbPayload(1).getZero
     )
+    myWbPayload(1).myExt.foreach(myExt => {
+      myExt.extraLastBackMmwValid.allowOverride
+      myExt.extraLastBackMmwValid.foreach(mmwValidItem => {
+        mmwValidItem := True
+      })
+      myExt.extraLastBackFwdCanDoIt.allowOverride
+      myExt.extraLastBackFwdCanDoIt.foreach(fwdCanDoItItem => {
+        fwdCanDoItItem := True
+      })
+    })
     when (cWb.up.isFiring) {
       cWb.up(modBackPayload) := myWbPayload(1)
     }
@@ -10824,12 +10834,12 @@ case class SnowHousePipeStageWriteBack(
         //})
         //cWb.down(modBackPayload) := myWbPayload(1).getZero
         cWb.down(modBackPayload).myExt.foreach(myExt => {
-          myExt.modMemWordValid.allowOverride
-          myExt.modMemWordValid.foreach(mmwValidItem => {
+          myExt.extraLastBackMmwValid.allowOverride
+          myExt.extraLastBackMmwValid.foreach(mmwValidItem => {
             mmwValidItem := False
           })
-          myExt.fwdCanDoIt.allowOverride
-          myExt.fwdCanDoIt.foreach(fwdCanDoItItem => {
+          myExt.extraLastBackFwdCanDoIt.allowOverride
+          myExt.extraLastBackFwdCanDoIt.foreach(fwdCanDoItItem => {
             fwdCanDoItItem := False
           })
         })
