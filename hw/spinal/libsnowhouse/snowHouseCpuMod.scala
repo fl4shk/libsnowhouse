069f1156a3a1d0173cfff6dcfebdb625f17d9d4c
fl4shk <fl4shk@users.noreply.github.com>
2026-03-11 12:41:30 -0500

    running more of the tests!

--- hw/spinal/libsnowhouse/snowHouseCpuMod.scala
+++ hw/spinal/libsnowhouse/snowHouseCpuMod.scala
@@ -7565,13 +7565,13 @@ object SnowHouseCpuWithDualRamSim extends App {
     false
   )
   val testIdxRange = Array[Int](
-    //0, //0,
+    0, //0,
     //1, 1,
     //2, 2,
     //3, 3,
-    4, 4,
+    //4, 4,
     //5, 5,
-    6, 6,
+    //6, 6,
     //7, 7,
     //8, //8,
     //9, //9,
