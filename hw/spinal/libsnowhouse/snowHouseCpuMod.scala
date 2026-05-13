c98fc49d811cd55fd5eacf2ebea4fcc23344a206
fl4shk <fl4shk@users.noreply.github.com>
2026-05-13 15:03:59 -0500

    finish runnigng all the tests. they all pass. `submodules/libcheesevoyage`: `git pull`

--- hw/spinal/libsnowhouse/snowHouseCpuMod.scala
+++ hw/spinal/libsnowhouse/snowHouseCpuMod.scala
@@ -7704,7 +7704,7 @@ object SnowHouseCpuWithDualRamSim extends App {
     false
   )
   val testIdxRange = Array[Int](
-    0, 0,
+    //0, //0,
     //1, //1,
     //2, 2,
     //3, //3,
@@ -7713,11 +7713,11 @@ object SnowHouseCpuWithDualRamSim extends App {
     //6, //6,
     //7, 7,
     //8, 8,
-    //9, //9,
+    //9, 9,
     //10, //10,
     ////11, 11,
     //12, 12,
-    //13, //13,
+    13, //13,
     //14, 14,
     15, 15,
     //16, 16
