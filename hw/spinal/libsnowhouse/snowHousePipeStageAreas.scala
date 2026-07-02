9b184fb5d1bff64d4979e4acb297f4e7a8c5f00d
fl4shk <fl4shk@users.noreply.github.com>
2026-07-02 17:01:54 -0500

    debugging for the purpose of RISC-V DOOM debugging!

--- hw/spinal/libsnowhouse/snowHousePipeStageAreas.scala
+++ hw/spinal/libsnowhouse/snowHousePipeStageAreas.scala
@@ -11656,6 +11656,8 @@ case class SnowHousePipeStageMem(
       cMem.up(pMem) := midModPayload(extIdxUp)
     } otherwise {
       cMem.up(pMem) := midModPayload(extIdxUp).getZero
+      cMem.up(pMem).instrCnt.shouldIgnoreInstr.allowOverride
+      cMem.up(pMem).instrCnt.shouldIgnoreInstr.last := True
       cMem.up(pMem).splitOp.allowOverride
       cMem.up(pMem).splitOp.setToDefault()
       cMem.up(pMem).gprIsZeroVec.allowOverride
