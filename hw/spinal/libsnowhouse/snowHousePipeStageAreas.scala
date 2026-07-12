9b4d63ab663afd10c3e4acd56bc059f1813f9938
fl4shk <fl4shk@users.noreply.github.com>
2026-07-12 12:15:05 -0500

    fmax debugging

--- hw/spinal/libsnowhouse/snowHousePipeStageAreas.scala
+++ hw/spinal/libsnowhouse/snowHousePipeStageAreas.scala
@@ -1067,7 +1067,7 @@ case class SnowHousePipeStageInstrFetch(
       pipeName="pipeStageIf",
       setWordFunc=mySetWordFunc,
       //initBigInt=initBigInt,
-      arrRamStyleAltera="M10K",//"MLAB",//"M10K"
+      arrRamStyleAltera="no_rw_check, M10K",//"MLAB",//"M10K"
       arrRamStyleXilinx="distributed",//"block"
     )
     WrPulseRdPipeRamSdpPipe(cfg=ramCfg)
