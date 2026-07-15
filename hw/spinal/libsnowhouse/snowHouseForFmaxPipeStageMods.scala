72b1ede0b17ddbb799bd5b6a833839cd57cdaaa0
fl4shk <fl4shk@users.noreply.github.com>
2026-07-15 13:30:50 -0500

    correctness debugging

--- hw/spinal/libsnowhouse/snowHouseForFmaxPipeStageMods.scala
+++ hw/spinal/libsnowhouse/snowHouseForFmaxPipeStageMods.scala
@@ -722,11 +722,11 @@ case class SnowHouseForFmaxPipeStageWriteBack(
     )
     io.dbgInfo.shouldIgnoreInstrAtRegFileWrite := (
       myWbPayload(1).instrCnt.shouldIgnoreInstr.last
-      || !cLink.up.isFiring
+      && cLink.up.isFiring
     )
     io.dbgInfo.myPsIdBubbleAtRegFileWrite := (
       myWbPayload(1).instrCnt.myPsIdBubble.last
-      || !cLink.up.isFiring
+      && cLink.up.isFiring
     )
     io.dbgInfo.encInstrAtRegFileWrite := (
       myWbPayload(1).encInstr.payload
