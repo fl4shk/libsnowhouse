0e0f69f69430f711802b6aa420c03ccbb6825b6d
fl4shk <fl4shk@users.noreply.github.com>
2026-07-18 21:39:54 -0500

    not sure if it actually executes instructions out of order...

--- hw/spinal/libsnowhouse/snowHouseCpuMod.scala
+++ hw/spinal/libsnowhouse/snowHouseCpuMod.scala
@@ -7917,8 +7917,8 @@ object SnowHouseCpuWithDualRamSim extends App {
     false
   )
   val testIdxRange = Array[Int](
-    0, 0,
-    //1, //1,
+    //0, 0,
+    1, //1,
     //////2, 2,
     //////3, //3,
     //4, //4,
