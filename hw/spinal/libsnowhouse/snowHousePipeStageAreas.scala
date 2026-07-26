1bd1e4db0e6915b5366359ced166f5b5e2fea664
fl4shk <fl4shk@users.noreply.github.com>
2026-07-26 07:32:57 -0500

    correctness debugging

--- hw/spinal/libsnowhouse/snowHousePipeStageAreas.scala
+++ hw/spinal/libsnowhouse/snowHousePipeStageAreas.scala
@@ -2582,7 +2582,7 @@ case class SnowHousePipeStagePreFwd(
         //  )
         //  || outp.regPcSetItCnt(1).lsb
         //)
-        && !outp.instrCnt.myPsIdBubble.head
+        //&& !outp.instrCnt.myPsIdBubble.head
         //&& (
         //  !outp.splitOp.opIsMemAccess
         //  || outp.inpDecodeExt.last.memAccessKind.asBits(1)
@@ -2618,7 +2618,7 @@ case class SnowHousePipeStagePreFwd(
           ////  )
           ////  || outp.regPcSetItCnt(1).lsb
           ////)
-          //&& !outp.instrCnt.myPsIdBubble.last
+          && !outp.instrCnt.myPsIdBubble.last
           //&& (
           //  !outp.splitOp.opIsMemAccess
           //  //|| outp.inpDecodeExt.last.memAccessKind.asBits(1)
