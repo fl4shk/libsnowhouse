83b74d98a8ac54433a5dcb65650d33e2b2faf7cc
fl4shk <fl4shk@users.noreply.github.com>
2026-07-27 18:32:20 -0500

    fmax debugging

--- hw/spinal/libsnowhouse/snowHouseUtilClasses.scala
+++ hw/spinal/libsnowhouse/snowHouseUtilClasses.scala
@@ -753,8 +753,8 @@ case class SnowHouseConfig(
           if (optScoreboard) (
             //6
             //4
-            //2
-            3
+            2
+            //3
             //1
             //2
           ) else (
