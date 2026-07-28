53778ee995e410ee17c00603b2bfed6cd9ae9dd3
fl4shk <fl4shk@users.noreply.github.com>
2026-07-28 14:57:17 -0500

    forwarding: correctness/fmax debugging

--- hw/spinal/libsnowhouse/snowHouseForFmaxPipeStageMods.scala
+++ hw/spinal/libsnowhouse/snowHouseForFmaxPipeStageMods.scala
@@ -1227,10 +1227,10 @@ case class SnowHouseForFmaxPipeStageScoreboardReadGprs(
   //  up=sLinkArr.last.down,
   //  down=Node(),
   //)
-  //sLinkArr += StageLink(
-  //  up=sLinkArr.last.down,
-  //  down=Node()
-  //)
+  sLinkArr += StageLink(
+    up=sLinkArr.last.down,
+    down=Node()
+  )
   //s2mLinkArr += S2MLink(
   //  up=sLinkArr.last.down,
   //  down=Node(),
