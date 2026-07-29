e0c41edefd2137837d876d3e32050b334cead0fc
fl4shk <fl4shk@users.noreply.github.com>
2026-07-29 12:52:23 -0500

    fmax debugging

--- hw/spinal/libsnowhouse/snowHouseForFmaxPipeStageMods.scala
+++ hw/spinal/libsnowhouse/snowHouseForFmaxPipeStageMods.scala
@@ -1217,12 +1217,12 @@ case class SnowHouseForFmaxPipeStageScoreboardReadGprs(
   //    temp
   //  }
   //)
-  //val sLinkArr = new ArrayBuffer[StageLink]()
-  ////val s2mLinkArr = new ArrayBuffer[S2MLink]()
-  //sLinkArr += StageLink(
-  //  up=cLink.down,
-  //  down=Node(),
-  //)
+  val sLinkArr = new ArrayBuffer[StageLink]()
+  //val s2mLinkArr = new ArrayBuffer[S2MLink]()
+  sLinkArr += StageLink(
+    up=cLink.down,
+    down=Node(),
+  )
   //sLinkArr += StageLink(
   //  up=sLinkArr.last.down,
   //  down=Node(),
@@ -1256,7 +1256,7 @@ case class SnowHouseForFmaxPipeStageScoreboardReadGprs(
   //  down=Node()
   //)
   linkArr += cLink
-  //linkArr ++= sLinkArr
+  linkArr ++= sLinkArr
   //linkArr ++= s2mLinkArr
   //linkArr += sLink
   //linkArr += s2mLink
@@ -1387,7 +1387,7 @@ case class SnowHouseForFmaxPipeStageScoreboardReadGprs(
   //  cLink.throwIt()
   //}
 
-  cLink.down.driveTo(io.down)(
+  sLinkArr.last.down.driveTo(io.down)(
     con=(outp, node) => {
       outp := node(pScoreboardReadGprsOutp)
     }
