43248c6c829dbcedf7b5b4cfd7067aae5530ee08
fl4shk <fl4shk@users.noreply.github.com>
2026-08-11 11:17:21 -0500

    IPC debugging

--- hw/spinal/libsnowhouse/snowHouseUtilClasses.scala
+++ hw/spinal/libsnowhouse/snowHouseUtilClasses.scala
@@ -748,18 +748,18 @@ case class SnowHouseConfig(
       case Some(myForFmaxCfg) => (
         (
           myForFmaxCfg.numPostExPreWbPipeStages
-          .max(
-            //0
-            //1
-            (
-              if (optScoreboard) (
-                //1
-                2
-              ) else (
-                0
-              )
-            )
-          )
+          //.max(
+          //  //0
+          //  //1
+          //  (
+          //    if (optScoreboard) (
+          //      //1
+          //      2
+          //    ) else (
+          //      0
+          //    )
+          //  )
+          //)
           //if (
           //  myForFmaxCfg.numPostExPreWbPipeStages <= 0//1
           //) (
@@ -774,8 +774,8 @@ case class SnowHouseConfig(
             //6
             //4
             //2
-            //3
-            1
+            3
+            //1
             //2
           ) else (
             0
